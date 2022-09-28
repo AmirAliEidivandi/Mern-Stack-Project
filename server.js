d17646c0dcb01af85d36d4451b0948e96d793410
AmirAliEidivandi <amirah648887@gmail.com>
2022-09-28 16:15:57 +0330

    adding middleware and config error

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
+// middlewares
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/root"));
@@ -16,6 +26,9 @@ app.all("*", (req, res) => {
         res.type("txt").send("404 Not Found");
     }
 });
+
+// errorHandler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
